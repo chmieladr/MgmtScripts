1a9ff64bbc1702b2d278db579755816e056fe64f
Adrian <120088418+chmieladr@users.noreply.github.com>
2024-09-09 03:15:00 +0200

    Added simple PowerShell scripts unblocker

---
+++ pwsh/unblock.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/pwsh
+# Script made by Adrian Chmiel (chmieladr)
+# Unblocks all of the downloaded PowerShell scripts
+
+# Run only once!
+# Do not use outside of this directory!
+# (might potentially unblock other unsafe files on your computer)
+
+# Change execution policy
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+
+# Get the list of all files
+$files = Get-ChildItem -File -Recurse
+
+# Unblock them
+foreach ($file in $files) {
+    Unblock-File $file.FullName
+}
+
+# Confirmation message
+Write-Host "All of the scripts have been unlocked!"
